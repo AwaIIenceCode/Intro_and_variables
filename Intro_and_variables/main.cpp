3495f4f96bc0e9052ce8814717254fef2d9174a3
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-11-11 01:00:53 +0200

    ex_3 is done

---
+++ Intro_and_variables/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <conio.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    cout << (char)201; 
+    cout << string(51, (char)205); 
+    cout << (char)187 << endl; 
+
+    cout << (char)186 << "\t\t\tSeasons\t\t\t    " << (char)186 << endl;
+
+    cout << (char)204; 
+    cout << string(12, (char)205) << (char)203; 
+    cout << string(12, (char)205) << (char)203; 
+    cout << string(12, (char)205) << (char)203; 
+    cout << string(12, (char)205) << (char)185 << endl; 
+
+    cout << (char)186 << "   Winter   " << (char)186 << "   Spring   " << (char)186 << "   Summer   " << (char)186 << "   Autumn   " << (char)186 << endl;
+
+    cout << (char)200; 
+    cout << string(12, (char)205) << (char)202; 
+    cout << string(12, (char)205) << (char)202; 
+    cout << string(12, (char)205) << (char)202; 
+    cout << string(12, (char)205) << (char)188 << endl; 
+
+    _getch();
+    return 0;
+}
+
+
+
+
